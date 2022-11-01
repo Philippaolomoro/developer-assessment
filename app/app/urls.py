09fbec5681a9a893f2cb7cc823ab34a42cef1fb5
Philippa Olomoro <olomoro.philippa@gmail.com>
2022-11-01 23:12:50 +0100

    create a fixture to import data into database

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
+from django.urls import include, re_path, path
+# from django.urls import path
+# from django.conf.urls import url
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r"graphql", GraphQLView.as_view(graphiql=True)),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
 ]
